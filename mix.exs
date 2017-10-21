eabdfe953baa0b70d8a365bcb69e1f040cf13477
Frank Hunleth <fhunleth@troodon-software.com>
2017-10-20 22:00:04 -0400

    Cleanup and refresh mix.{exs,lock}

--- mix.exs
+++ mix.exs
@@ -2,27 +2,29 @@ defmodule NervesSystemVultr.Mixfile do
   use Mix.Project
 
   @version Path.join(__DIR__, "VERSION")
-    |> File.read!
-    |> String.trim
+           |> File.read!()
+           |> String.trim()
 
   def project do
-    [app: :nerves_system_vultr,
-     version: @version,
-     elixir: "~> 1.4",
-     compilers: Mix.compilers ++ [:nerves_package],
-     description: description(),
-     package: package(),
-     deps: deps(),
-     aliases: ["deps.precompile": ["nerves.env", "deps.precompile"]]]
+    [
+      app: :nerves_system_vultr,
+      version: @version,
+      elixir: "~> 1.4",
+      compilers: Mix.compilers() ++ [:nerves_package],
+      description: description(),
+      package: package(),
+      deps: deps(),
+      aliases: ["deps.precompile": ["nerves.env", "deps.precompile"]]
+    ]
   end
 
   def application do
-   []
+    []
   end
 
   defp deps do
     [
-      {:nerves, "~> 0.7", runtime: false },
+      {:nerves, "~> 0.7", runtime: false},
       {:nerves_system_br, "~> 0.14", runtime: false},
       {:nerves_toolchain_x86_64_unknown_linux_musl, "~> 0.11.0", runtime: false}
     ]
@@ -35,11 +37,22 @@ defmodule NervesSystemVultr.Mixfile do
   end
 
   defp package do
-    [maintainers: ["Frank Hunleth"],
-     files: ["LICENSE", "mix.exs", "nerves_defconfig", "nerves.exs", "README.md",
-             "VERSION", "rootfs_overlay", "linux-4.11.defconfig", "grub.cfg",
-             "post-createfs.sh"],
-     licenses: ["Apache 2.0"],
-     links: %{"Github" => "https://github.com/fhunleth/nerves_system_vultr"}]
+    [
+      maintainers: ["Frank Hunleth"],
+      files: [
+        "LICENSE",
+        "mix.exs",
+        "nerves_defconfig",
+        "nerves.exs",
+        "README.md",
+        "VERSION",
+        "rootfs_overlay",
+        "linux-4.11.defconfig",
+        "grub.cfg",
+        "post-createfs.sh"
+      ],
+      licenses: ["Apache 2.0"],
+      links: %{"Github" => "https://github.com/fhunleth/nerves_system_vultr"}
+    ]
   end
 end
