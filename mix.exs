72a6d23372531dc547610d0c7c04cc7b461b9690
Justin Schneck <jschneck@mac.com>
2019-07-02 15:53:42 -0400

    nerves_system_br: bump to v1.8.2

--- mix.exs
+++ mix.exs
@@ -48,7 +48,7 @@ defmodule NervesSystemVultr.MixProject do
   defp deps do
     [
       {:nerves, "~> 1.4", runtime: false},
-      {:nerves_system_br, "1.8.0", runtime: false},
+      {:nerves_system_br, "1.8.2", runtime: false},
       {:nerves_toolchain_x86_64_unknown_linux_musl, "1.2.0", runtime: false},
       {:nerves_system_linter, "~> 0.3.0", runtime: false},
       {:ex_doc, "~> 0.18", only: [:dev, :test], runtime: false}
